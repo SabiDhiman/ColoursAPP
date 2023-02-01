01c887b66e3dd9940080076c1ffc6f8b70a78b10
SabiDhiman <sabi98@live.co.uk>
2023-02-01 17:29:05 +0000

    added logic for navigation when cell is clicked

--- ColoursTableViewController.swift
+++ ColoursTableViewController.swift
@@ -1,32 +1,41 @@
-//
-//  ColoursTableViewController.swift
-//  ColoursApp
-//
-//  Created by Dhiman, Sabi (Consumer Servicing & Engagement Platform) on 01/02/2023.
-//
+
 
 import UIKit
 
 class ColoursTableViewController: UIViewController {
-
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "toColoursDetailVC", sender: nil)
-    }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+//for code organisation we move the two functions into an extension with viewdidload at the top
+extension ColoursTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+      //how many rows to show
+    //will also be the number of colours in the array
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+        
+        // initalising blank cells
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toColoursDetailVC", sender: nil)
+        
+        //performSegue - perform the action of navigating to the third storyboard for any cell that is clicked
+    }
+    
+}
+
+//delegate methods - every time a cell appears on the screen the delegate function is triggered
+//delegate functions wait to be called and sit on standby
